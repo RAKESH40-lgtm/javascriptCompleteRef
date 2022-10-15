7074fb218e5f63d7152a8571f5271ca0e3ab8c75
RAKESH40-lgtm <ram19042k@protonmail.com>
2022-10-15 18:08:52 +0530

    till asyn-await

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,6 +1,6 @@
 //making htttp request using fetch API
 //fetch:easy to fetch similar to the xhrHTTPrequest() is a builtin which does every thing as we did in xhrHTTPrequest and by default it will be get request As fetch return promise we can do chaining as shown below
-const URL = "https://jsonplaceholder.typicode.com/posts"
+// const URL = "https://jsonplaceholder.typicode.com/posts"
 //in fetch it will do all process from background as it done by xhrHTTPRequest() first it return  response which can be convert to json() which is another promise so chaining can be done to get the data as shown below 
 // console.log(fetch(URL))//return a response
 //default get method .catch()cannot be 
@@ -10,7 +10,6 @@ const URL = "https://jsonplaceholder.typicode.com/posts"
 //     }else{
 //         throw new Error("Something went wrong")
 //     }
-
 // }).then((data)=>{
 //     console.log(data)
 // }).catch((err)=>{
@@ -30,3 +29,24 @@ const URL = "https://jsonplaceholder.typicode.com/posts"
 // })
 //   .then((response) => response.json())
 //   .then((json) => console.log(json));
+//async and await:.then() is consuming promise  and async will return promise and await is used in fetch and convert to json and it wait till the async resolve and then then it works same in async  can write any times await() in program and here also catch doesn't we have to handle it externally by throw error and in background async-await works asynchronous and async will do the promises and it return the promise till wait it
+const URL = "https://jsonplaceholder.typicode.com/posts"
+// fetch(URL).then((res)=>{
+//     return res.json()
+// }).then((data)=>{
+//     console.log(data)
+// })
+async function getPosts(){
+    const res=await fetch(URL);
+    const data=await(res.json())
+    return data
+}
+getPosts().then((data)=>{
+    console.log(data)
+}).catch((err)=>{
+    console.log(err)
+})
+//in arrow function we write async as located
+// const getPosts=async()=>{
+
+// }
